24b97fc2f4a47566cd2c09553d4c4b8a3f9f9007
yoonwoo4429 <yoonwoo4429@naver.com>
2023-09-16 13:35:45 +0900

    fix: fix variable name

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// add router 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/books', require('./routes/books'))
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // index.ejs라는 페이지를 출력해라 
   res.render('index', { title: '홈페이지', pageName:'home.ejs' });
 });
 
