4140982a6e5647c5a299480057ecfa55b1d7512a
yoonwoo4429 <yoonwoo4429@naver.com>
2023-09-23 16:17:02 +0900

    add: locals/search.ejs and books/modal.ejs

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/books', require('./routes/books'))
-
+app.use('/books', require('./routes/books'));
+app.use('/locals', require('./routes/locals'));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
