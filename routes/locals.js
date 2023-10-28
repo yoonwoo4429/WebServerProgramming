97781c1fe97c52a52e88cb9df6e41801ea9dd529
김윤우 <>
2023-10-28 15:27:52 +0900

    9주차: 즐겨찾기 추가

--- routes/locals.js
+++ routes/locals.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: '지역검색', pageName:'locals/search.ejs' });
 });
+router.get('/favorite', function(req, res) {
+  res.render('index', { title: '즐겨찾기', pageName:'locals/favorite.ejs' });
+});
 
 module.exports = router;
